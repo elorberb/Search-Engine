5fe0e3eda56357c7b521d99aa87be81118af8d98
elorberb <etay.lorberboym@intel.com>
2023-01-15 22:17:46 +0200

    update tests.py

--- src/tests.py
+++ src/tests.py
@@ -1,110 +1,134 @@
 import unittest
 from backend import *
 from bm25 import *
+from time import time
+from metrics import *
+from search_functions import *
+from retrievel_functions import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        print(DL)
-        self.assertEqual(True, True)  # add assertion here
-
-    def test_get_relevant_docs_id(self):
-        query = """
-        Verse 1:
-Inverted index, oh so fine
-It stores words, one at a time
-A little bit of structure, goes a long way
-Makes searching lightning fast, hooray!
-
-Chorus:
-Inverted index, it's the way to go
-Find what you need, in a row
-Five words, appear at least twice
-Inverted index, it's nice!
-
-Verse 2:
-Words are mapped, to their page
-So you can find, in a rage
-What you're looking for, in a jiff
-Inverted index, it's so swift!
-
-Chorus:
-Inverted index, it's the way to go
-Find what you need, in a row
-Five words, appear at least twice
-Inverted index, it's nice!
-
-Bridge:
-Searching for something, deep in a book
-Inverted index, helps you look
-For what you need, without delay
-Inverted index, makes it all okay!
-
-Chorus:
-Inverted index, it's the way to go
-Find what you need, in a row
-Five words, appear at least twice
-Inverted index, it's nice!
-        """
-        query = tokenize(query)
-        ids = get_doc_id_by_count(query, title_index, SRC_PATH)
-        print(ids[:5])
-        self.assertEqual(True, True)  # add assertion here
 
     def test_search_body(self):
-        query = "similarity laws must obeyed when constructing aeroelastic models of heated high speed aircraft"
-        vals = calc_search(query)
-        print(vals[:5])
+        query = "best marvel movie"
+        t_start = time()
+        vals = calc_search_body(query)
+        duration = time() - t_start
+        print(vals)
+        print(f'duration: {duration}')
         self.assertEqual(True, True)  # add assertion here
 
     def test_search_title(self):
-        query = "similarity laws must obeyed when constructing aeroelastic models of heated high speed aircraft"
+        query = "best marvel movie"
+        t_start = time()
         vals = calc_search_title(query)
+        duration = time() - t_start
         print(vals)
+        print(f'duration: {duration}')
         self.assertEqual(True, True)  # add assertion here
 
     def test_search_anchor(self):
-        query = 'hello world'
+        query = 'best marvel movie'
         vals = calc_search_anchor(query)
         print(vals)
         self.assertEqual(True, True)  # add assertion here
 
-    def test_id2title(self):
-        print(len(id2title.keys()))
-        self.assertEqual(True, True)
+    def test_calc_search_body(self):
+        with open('../extra_files/queries_train.json', 'rt') as f:
+            queries = json.load(f)
+        qs_res = []
+        for q, true_wids in queries.items():
+            duration, ap = None, None
+            true_wids = list(map(int, true_wids))
+            t_start = time()
+            pred_wids = calc_search_body(q, text_index)
+            duration = time() - t_start
+            ap = average_precision(true_wids, pred_wids)
+            qs_res.append((q, duration, ap))
+        second_values = [x[2] for x in qs_res]
+        mean = sum(second_values) / len(second_values)
+        print(mean)
+        print(qs_res)
+
+    def test_calc_search_body_stem(self):
+        with open('../extra_files/queries_train.json', 'rt') as f:
+            queries = json.load(f)
+        qs_res = []
+        for q, true_wids in queries.items():
+            duration, ap = None, None
+            true_wids = list(map(int, true_wids))
+            t_start = time()
+            pred_wids = calc_search_body_stem(q)
+            duration = time() - t_start
+            ap = average_precision(true_wids, pred_wids)
+            qs_res.append((q, duration, ap))
+        second_values = [x[2] for x in qs_res]
+        mean = sum(second_values) / len(second_values)
+        print(mean)
+        print(qs_res)
 
     def test_bm25(self):
         query = "best marvel movie"
-        query = tokenize(query)
-        bm25_text = BM25(text_index, DL)
-        bm25_title = BM25(title_index, DL)
-        bm25_score = bm25_text.search(query)
-        bm25_score_title = bm25_title.search(query)
-        print(bm25_score)
-        print(bm25_score_title)
-        self.assertEqual(True, True)
+        t_start = time()
+        res = calc_bm25(query, text_index, TEXT_BUCKET, DL)
+        duration = time() - t_start
+        print(res)
+        print(duration)
+        self.assertEqual(True, True)  # add assertion here
 
-    def test_final_bm25(self):
-        query = "best marvel movie"
-        scores = calc_bm25(query, text_index)
-        print(scores)
-        self.assertEqual(True, True)
+    def test_calc_search_anchor(self):
+        with open('../extra_files/queries_train.json', 'rt') as f:
+            queries = json.load(f)
+        qs_res = []
+        for q, true_wids in queries.items():
+            duration, ap = None, None
+            true_wids = list(map(int, true_wids))
+            t_start = time()
+            pred_wids = calc_search_anchor(q)
+            duration = time() - t_start
+            ap = average_precision(true_wids, pred_wids)
+            qs_res.append((q, duration, ap))
+        second_values = [x[2] for x in qs_res]
+        mean = sum(second_values) / len(second_values)
+        print(mean)
+        print(qs_res)
+        self.assertEqual(True, True)  # add assertion here
 
-    def test_eval_retrieve(self):
-        evaluate_retrieve()
-        self.assertEqual(True, True)
+    def test_calc_search_title(self):
+        with open('../extra_files/queries_train.json', 'rt') as f:
+            queries = json.load(f)
+        qs_res = []
+        for q, true_wids in queries.items():
+            duration, ap = None, None
+            true_wids = list(map(int, true_wids))
+            t_start = time()
+            pred_wids = calc_search_title(q)
+            duration = time() - t_start
+            ap = average_precision(true_wids, pred_wids)
+            qs_res.append((q, duration, ap))
+        second_values = [x[2] for x in qs_res]
+        mean = sum(second_values) / len(second_values)
+        print(mean)
+        print(qs_res)
+        self.assertEqual(True, True)  # add assertion here
 
-    def test_word_embed(self):
-        model = KeyedVectors.load_word2vec_format("../crawl-300d-2M.vec")
-        print(type(model))
-        self.assertEqual(True, True)
-
-    def test_search(self):
-        query = "best marvel movie"
-        scores = search(query)
-        print(scores)
-        self.assertEqual(True, True)
-
+    def test_calc_search_anchor_double(self):
+        with open('../extra_files/queries_train.json', 'rt') as f:
+            queries = json.load(f)
+        qs_res = []
+        for q, true_wids in queries.items():
+            duration, ap = None, None
+            true_wids = list(map(int, true_wids))
+            t_start = time()
+            pred_wids = calc_search_anchor_double(q)
+            duration = time() - t_start
+            ap = average_precision(true_wids, pred_wids)
+            qs_res.append((q, duration, ap))
+        second_values = [x[2] for x in qs_res]
+        mean = sum(second_values) / len(second_values)
+        print(mean)
+        print(qs_res)
+        self.assertEqual(True, True)  # add assertion here
 
 
 if __name__ == '__main__':
